1868847394699af6ab6e72022114f8e26483582e
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2019-12-10 13:04:35 +0100

    Install server dependencies, create server config file and manage file to start server.

---
+++ src/server/manage.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from legarda.api import create_app, db
+
+app = create_app(os.getenv('APP_ENV') or 'dev')
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def run():
+    app.run()
+
+
+if __name__ == '__main__':
+    manager.run()
